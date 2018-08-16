317c3b1dcc78073576b8ab23c36862d37590654c
Claire Harmer <claire.f.harmer@gmail.com>
2018-08-16 21:47:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.transaction = { item: title, price: price, quantity: quantity}
+    self.total += price * quantity
+    quantity.times do
+    self.items << title
+  end
+end
